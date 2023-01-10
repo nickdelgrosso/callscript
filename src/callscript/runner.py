9ae7020fb6d96423e10f01d11a37d9cdeae6c109
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-01-10 15:34:49 +0100

    specify variable names for outputs

--- src/callscript/runner.py
+++ src/callscript/runner.py
@@ -2,18 +2,26 @@ from typing import Any, List, Dict
 from redbaron import RedBaron, AssignmentNode
 
 
-def get_output_var_names(code: str, substr: str = ":output:") -> List[str]:
+def get_output_var_names(code: str, substr: str = "output") -> List[str]:
     red = RedBaron(code)
     nodes = red.node_list
     output_comment_nodes = nodes.find_all('comment', value = lambda s: substr in s)
-    output_line_nums = (node.absolute_bounding_box.top_left.line for node in output_comment_nodes)
+    comments = [node.dumps() for node in output_comment_nodes]
+    output_line_nums = [node.absolute_bounding_box.top_left.line for node in output_comment_nodes]
     output_nodes = [red.at(line_num) for line_num in output_line_nums]
     assert all(isinstance(node, AssignmentNode) for node in output_nodes)
     output_names = [node.target.dumps() for node in output_nodes]
-    return output_names
+
+    full_names = {}
+    for output_name, comment in zip(output_names, comments):
+        cmd, *newname = comment.split(':', 1)
+        full_name = newname[0] if newname else output_name
+        full_names[output_name] = full_name
+
+    return full_names
 
 
-def replace_inputs(code: str, replacements: Dict[str, Any], substr: str = ":input:") -> str:
+def replace_inputs(code: str, replacements: Dict[str, Any], substr: str = "input") -> str:
     red = RedBaron(code)
     nodes = red.node_list
     input_comment_nodes = nodes.find_all('comment', value=lambda s: substr in s)
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,13 +6,23 @@ from callscript.runner import get_output_var_names, replace_inputs
 def test_get_output_names():
     script = dedent("""
     start = "hello"
-    x = 3 # :output:
+    x = 3 # output
     y = 5 # This code does nothing
-    final = "done!"  # :output:
+    final = "done!"  # output
     """)
-    expected_outputs = ['x', 'final']
-    outputs = get_output_var_names(script, ':output:')
+    expected_outputs = {'x': 'x', 'final': 'final'}
+    outputs = get_output_var_names(script)
     assert outputs == expected_outputs
+
+
+def test_specify_output_variables_gets_new_varnames():
+    script = dedent("""
+        x = 3 # output:First
+        y = 5 # output:Second
+    """)
+    outputs = get_output_var_names(script)
+    assert outputs == {'x': 'First', 'y': 'Second'}
+
 
 
 def test_replace_input_variables():
@@ -27,5 +37,6 @@ def test_replace_input_variables():
     """)
     result = replace_inputs(code, replacement, ':input:')
     assert result == expected_result
+
 
 
