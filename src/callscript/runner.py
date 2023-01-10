81a9b3395bfca23762e6ce98273a61722229ca52
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-01-10 13:27:34 +0100

    to generator comprehension

--- src/callscript/runner.py
+++ src/callscript/runner.py
@@ -6,7 +6,7 @@ def get_output_var_names(code: str, substr: str = ":output:") -> List[str]:
     red = RedBaron(code)
     nodes = red.node_list
     output_comment_nodes = nodes.find_all('comment', value = lambda s: substr in s)
-    output_line_nums = [node.absolute_bounding_box.top_left.line for node in output_comment_nodes]
+    output_line_nums = (node.absolute_bounding_box.top_left.line for node in output_comment_nodes)
     output_nodes = [red.at(line_num) for line_num in output_line_nums]
     assert all(isinstance(node, AssignmentNode) for node in output_nodes)
     output_names = [node.target.dumps() for node in output_nodes]
@@ -17,10 +17,11 @@ def replace_inputs(code: str, replacements: Dict[str, Any], substr: str = ":inpu
     red = RedBaron(code)
     nodes = red.node_list
     input_comment_nodes = nodes.find_all('comment', value=lambda s: substr in s)
-    input_line_nums = [node.absolute_bounding_box.top_left.line for node in input_comment_nodes]
-    input_nodes = [red.at(line_num) for line_num in input_line_nums]
+    input_line_nums = (node.absolute_bounding_box.top_left.line for node in input_comment_nodes)
+    input_nodes = (red.at(line_num) for line_num in input_line_nums)
     for node in input_nodes:
         node.value = str(replacements[node.target.dumps()])
     new_code = red.dumps()
     return new_code
     
+
