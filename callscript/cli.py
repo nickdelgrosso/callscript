54bed1715fec6385c2a64e5a7e2a26fc80833beb
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2023-01-24 17:26:52 +0100

    cli in lib

---
+++ callscript/cli.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import List
+
+from callscript import modify_code
+
+
+def main(args: List[str] = None):
+    parser = ArgumentParser(description="Extract Script into a Module using Comment Tags")
+    parser.add_argument('script', help="Path to the script")
+
+    args = parser.parse_args(args=args)
+
+    code = Path(args.script).read_text()
+    meta = modify_code(code=code)
+    new_code = meta['code']
+    print(new_code)
+
+
+if __name__ == '__main__':
+    main()
